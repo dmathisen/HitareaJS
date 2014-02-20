dac7860d5ce89da030049c4dd440a78831929a3d
U-MIKFS\dmathisen <dan.mathisen1@gmail.com>
2014-02-20 16:26:43 -0500

    initial commit

---
+++ js/hitarea.js
@@ -0,0 +1,29 @@
+(function($) {
+    $.fn.hitarea = function(fn_enter, fn_leave, options) {
+        var el = $(this);
+
+        var settings = $.extend({
+            radius: 20
+        }, options);
+
+        // wrap element in hit area
+        el.wrap('<div class="hitarea-wrapper"><div class="hitarea">');
+
+        // get wrapper and apply
+        var wrapper = el.closest('.hitarea-wrapper');
+        var hitElem = $('.hitarea', wrapper);
+
+        // apply correct positioning to hitElem
+        hitElem.css({
+            'width': el.outerWidth(),
+            'height': el.outerHeight(),
+            'padding': settings.radius,
+            'margin-top': -(settings.radius),
+            'margin-right': -(settings.radius),
+            'margin-bottom': -(settings.radius),
+            'margin-left': -(settings.radius)
+        });
+
+        hitElem.hover(fn_enter, fn_leave);
+    };
+}(jQuery));
