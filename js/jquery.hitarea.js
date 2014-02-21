2bcb5b325ba531d1b934299680d13d0df0e63be1
U-MIKFS\dmathisen <dan.mathisen1@gmail.com>
2014-02-21 12:31:19 -0500

    adding comments to js

--- js/jquery.hitarea.js
+++ js/jquery.hitarea.js
@@ -1,6 +1,6 @@
 (function($) {
     $.fn.hitarea = function(fn_enter, fn_leave, options) {
-        var el = $(this);
+        var el = $(this); // targetted element
 
         var settings = $.extend({
             // default settings
@@ -10,12 +10,13 @@
             endOn: "enter"
         }, options);
 
-        // wrap element in hit area
+        // wrap element in hitarea tags
         el.wrap('<div class="hitarea-wrapper"><div class="hitarea">');
 
-        // get wrapper and apply
-        var wrapper = el.closest('.hitarea-wrapper');
-        var hitElem = $('.hitarea', wrapper);
+        // get parent wrapper and use it as hitElem's context
+        // this allows for targeting multiple hitaera elements without collision
+        var context = el.closest('.hitarea-wrapper');
+        var hitElem = $('.hitarea', context);
 
         // apply correct positioning to hitElem
         hitElem.css({
@@ -33,14 +34,16 @@
 
 
         hitElem.hover(
+            // mouse enter
             function() {
-                fn_enter(el[0]);
+                fn_enter(el[0]); // trigger enter function passing the element as the only parameter
 
                 if (settings.repeat === false && settings.endOn === "enter")
                     hitElem.unbind('mouseenter mouseleave');
             },
+            // mouse leave
             function() {
-                fn_leave(el[0]);
+                fn_leave(el[0]); // trigger leave function passing the element as the only parameter
 
                 if (settings.repeat === false && settings.endOn === "leave")
                     hitElem.unbind('mouseenter mouseleave');
